08ec1e0dafdf453e8995b499c6e9c57a0a129741
ramcste <fazlayrabbey26@gmail.com>
2015-05-12 17:00:47 +0600

    First Commit

---
+++ AccountOperation/Account Operation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        
+
+        Account account=new Account();
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            
+
+            //aCustomer.customerName=accountNumberTextBox.Text;
+            //aCustomer.accountNumber = customerNameTextBox.Text;
+
+            account.customerName = customerNameTextBox.Text;
+            account.accountNumber = accountNumberTextBox.Text;
+            MessageBox.Show("Account Created");
+
+            ClearText();
+
+            //accountNumberTextBox.Text = String.Empty;
+            //customerNameTextBox.Text = String.Empty;
+        }
+
+        public void ClearText()
+        {
+            accountNumberTextBox.Text = String.Empty;
+            customerNameTextBox.Text = String.Empty;
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Customer Name"+ account.customerName +"\nAccount Number"+ account.accountNumber+"\nBalance"+ account.balance);
+            //MessageBox.Show("Customer Current Balance"+ amount);
+        }
+
+        
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(amountTextBox.Text);
+            if (amount>0)
+            {
+                account.Diposit(amount);
+                MessageBox.Show("Diposited");
+            }
+            else
+            {
+                MessageBox.Show("Not Possible");
+            }
+
+            //aCustomer.deposit();
+            ////aCustomer.
+            //// amount+= Convert.ToDouble(amountTextBox.Text.ToString());
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(amountTextBox.Text);
+            if (account.balance-amount>0 && amount>0)
+            {
+                account.withdraw(amount);
+                MessageBox.Show("Withdrawn");
+            }
+            else
+            {
+                MessageBox.Show("Not enough memory");
+            }
+            
+            
+            //aCustomer.withdraw();
+            ////amount-=Convert.ToDouble(amountTextBox.Text.ToString());
+        }
+
+
+    }
+}
---
+++ AccountOperation/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperation
+{
+    public class Account
+    {
+        public string customerName;
+        public string accountNumber;
+        public double balance;
+        public double amount;
+
+
+        public double Diposit(double amount)
+        {
+            return balance+= amount;
+        }
+
+        public double withdraw(double amount)
+        {
+            return balance -= amount;
+        }
+
+
+    }
+}
